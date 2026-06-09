5fee4b065705a2755b0827c65bb18b21c53a9488
yuan0 <yuan0.2029@gmail.com>
2026-06-09 17:14:18 +0800

    Add BLOGS section & blogs.html & blogs.css

--- index.js
+++ index.js
@@ -8,9 +8,13 @@ function toggleMenu() {
   document.body.classList.toggle("menu-open");
 }
 
-// 事件監聽器（白話翻譯版：當使用者點擊漢堡選單按鈕，我要做什麼事情）
 menuBtn.addEventListener("click", toggleMenu);
 
 if (mask) {
   mask.addEventListener("click", toggleMenu);
+}
+
+// 導向別的網頁
+function myFunction() {
+  window.location.href = "./blogs.html";
 }
